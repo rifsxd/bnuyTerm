fcd0f5e758789b835e1b4cdb501a4082004e4ae8
Rifat Azad <33044977+rifsxd@users.noreply.github.com>
2023-11-29 11:37:50 +0600

    new renewed minimal code

--- src/bnuy.cpp
+++ src/bnuy.cpp
@@ -1,102 +1,60 @@
 #include <gtk/gtk.h>
 #include <vte/vte.h>
-#include <stdlib.h>
 
-static GtkWidget *create_terminal() {
-    GtkWidget *terminal;
-    terminal = vte_terminal_new();
-    return terminal;
-}
+static void activate(GtkApplication* app, gpointer user_data) {
+    GtkWidget *window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "BnuyTerm");
+    gtk_window_set_default_size(GTK_WINDOW(window), 600, 400);
 
-static void update_terminal_colors(GtkWidget *terminal) {
-    GdkRGBA bg_color, fg_color;
+    VteTerminal *terminal = VTE_TERMINAL(vte_terminal_new());
+    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(terminal));
 
-    // Get the background and foreground colors from the current GTK theme
-    gtk_style_context_get_background_color(gtk_widget_get_style_context(terminal), GTK_STATE_FLAG_NORMAL, &bg_color);
-    gtk_style_context_get_color(gtk_widget_get_style_context(terminal), GTK_STATE_FLAG_NORMAL, &fg_color);
+    // Set a retro Fallout color scheme
+    GdkRGBA fg_color, bg_color;
+    gdk_rgba_parse(&fg_color, "#00FF00"); // Bright green for foreground
+    gdk_rgba_parse(&bg_color, "#000000"); // Black for background
+    vte_terminal_set_color_foreground(terminal, &fg_color);
+    vte_terminal_set_color_background(terminal, &bg_color);
 
-    // Set the background and foreground colors for the terminal
-    vte_terminal_set_color_background(VTE_TERMINAL(terminal), &bg_color);
-    vte_terminal_set_color_foreground(VTE_TERMINAL(terminal), &fg_color);
-}
+    // Set a vintage font
+    PangoFontDescription *font_desc = pango_font_description_from_string("Monospace 12");
+    vte_terminal_set_font(terminal, font_desc);
+    pango_font_description_free(font_desc);
 
-static void style_set_callback(GtkWidget *widget, GtkStyleProvider *previous, gpointer user_data) {
-    // Callback function to handle style changes
-    update_terminal_colors(widget);
-}
+    // Corrected type for spawn_flags
+    GSpawnFlags spawn_flags = static_cast<GSpawnFlags>(0);
 
-static void spawn_shell(GtkWidget *terminal) {
-    GPid child_pid;
-    const char *user_shell = getenv("SHELL");
+    // Get the default user shell
+    gchar *user_shell = getenv("SHELL");
 
-    if (!user_shell) {
-        // Default to /bin/bash if SHELL environment variable is not set
-        user_shell = "/bin/bash";
-    }
-
-    char *argv[] = {(char *)user_shell, NULL};
-    char *envp[] = {NULL};
-
-    vte_terminal_spawn_async(VTE_TERMINAL(terminal),
+    // Spawn the user's default shell
+    gchar **envp = g_get_environ();
+    gchar **command = g_strsplit(user_shell, " ", -1);
+    vte_terminal_spawn_async(terminal,
                              VTE_PTY_DEFAULT,
-                             NULL,   // working directory
-                             argv,   // command
-                             envp,   // environment
-                             G_SPAWN_DEFAULT, // spawn flags
-                             NULL,   // child setup function
-                             NULL,   // child setup data
-                             NULL,   // child pid
-                             -1,     // timeout (use default)
-                             NULL,   // cancellable
-                             NULL,   // callback
-                             NULL);  // user data
-}
+                             NULL,         // working directory
+                             command,      // command
+                             envp,         // environment variables
+                             spawn_flags,  // corrected spawn flags
+                             NULL,         // child setup function
+                             NULL,         // user data
+                             NULL,         // child setup data destroy
+                             -1,           // timeout (use default)
+                             NULL,         // cancellable
+                             NULL,         // callback
+                             NULL);        // user data destroy
 
-static gboolean check_theme_change(gpointer user_data) {
-    GtkWidget *terminal = GTK_WIDGET(user_data);
-    GdkRGBA bg_color, fg_color;
-
-    gtk_style_context_get_background_color(gtk_widget_get_style_context(terminal), GTK_STATE_FLAG_NORMAL, &bg_color);
-    gtk_style_context_get_color(gtk_widget_get_style_context(terminal), GTK_STATE_FLAG_NORMAL, &fg_color);
-
-    vte_terminal_set_color_background(VTE_TERMINAL(terminal), &bg_color);
-    vte_terminal_set_color_foreground(VTE_TERMINAL(terminal), &fg_color);
-
-    return G_SOURCE_CONTINUE;
-}
-
-
-static void activate(GtkApplication *app, gpointer user_data) {
-    GtkWidget *window;
-    GtkWidget *terminal;
-
-    window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "bnuyTerm");
-    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
-
-    terminal = create_terminal();
-    gtk_container_add(GTK_CONTAINER(window), terminal);
-
-    // Connect the style-set signal to the callback function
-    g_timeout_add_seconds(1, (GSourceFunc)check_theme_change, terminal);
-
-
-    // Set initial colors
-    update_terminal_colors(terminal);
-
-    spawn_shell(terminal);
+    g_strfreev(envp);
+    g_strfreev(command);
 
     gtk_widget_show_all(window);
 }
 
 int main(int argc, char **argv) {
-    GtkApplication *app;
-    int status;
-
-    app = gtk_application_new("com.bnuy.Term", G_APPLICATION_DEFAULT_FLAGS);
+    GtkApplication *app = gtk_application_new("org.bnuy.term", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 
-    status = g_application_run(G_APPLICATION(app), argc, argv);
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
 
     return status;
